bc8ec7ec9df20eb2ef5487bc31ea0700743eb747
frchp <achille.petitjean@gmail.com>
2025-02-04 16:47:56 +0100

    Remove fetch of cube-l5

--- cmake/dependencies.cmake
+++ cmake/dependencies.cmake
@@ -3,14 +3,15 @@ include(FetchContent)
 message("dependencies.cmake")
 
 list(APPEND CMAKE_MESSAGE_INDENT "|")
-
-message("cube-l5 fetched")
-FetchContent_Declare(
-  cube-l5
-  GIT_REPOSITORY https://github.com/STMicroelectronics/STM32CubeL5.git
-  GIT_TAG v1.5.1
-)
-FetchContent_makeAvailable(cube-l5)
+if(FETCH_VENDOR_UPDATE)
+  message("cube-l5 fetched")
+  FetchContent_Declare(
+    cube-l5
+    GIT_REPOSITORY https://github.com/STMicroelectronics/STM32CubeL5.git
+    GIT_TAG v1.5.1
+  )
+  FetchContent_makeAvailable(cube-l5)
+endif()
 
 message("FreeRTOS fetched")
 FetchContent_Declare(
